9ed713fd81827c9e369487a9414e715d23263aff
brvcelose <contatobruce90@gmail.com>
2024-12-17 01:38:18 -0300

    EgressoServiceTest refactoring

--- src/test/java/com/ufma/PortalEgresso/service/EgressoServiceTest.java
+++ src/test/java/com/ufma/PortalEgresso/service/EgressoServiceTest.java
@@ -3,6 +3,8 @@ package com.ufma.PortalEgresso.service;
 import com.ufma.PortalEgresso.exception.BuscaVaziaRunTime;
 import com.ufma.PortalEgresso.exception.RegraNegocioRunTime;
 import com.ufma.PortalEgresso.model.entity.*;
+import com.ufma.PortalEgresso.model.repo.CargoRepo;
+import com.ufma.PortalEgresso.model.repo.CursoRepo;
 import com.ufma.PortalEgresso.model.repo.EgressoRepo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -15,7 +17,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,22 +26,16 @@ import java.util.UUID;
 public class EgressoServiceTest {
 
     @Autowired
-    CursoService cursoService;
+    private EgressoService service;
 
     @Autowired
-    CursoEgressoService cursoEgressoService;
+    private EgressoRepo repo;
 
     @Autowired
-    CoordenadorService coordenadorService;
+    private CursoRepo cursoRepo;
 
     @Autowired
-    CargoService cargoService;
-
-    @Autowired
-    EgressoService service;
-
-    @Autowired
-    EgressoRepo repo;
+    private CargoRepo cargoRepo;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -91,30 +86,6 @@ public class EgressoServiceTest {
         Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.salvar(egresso), "O e-mail já está cadastrado. Por favor, utilize um e-mail diferente");
     }
 
-
-    @Test
-    @Transactional
-    public void deveVerificarSalvarOEgresso() {
-        Egresso egresso = new Egresso();
-        egresso.setNome("nome teste");
-        egresso.setDescricao("Descricao teste");
-        egresso.setEmail("salvar@teste.com");
-        egresso.setSenha("senha teste");
-
-        Egresso salvo = service.salvar(egresso);
-
-        //Verificação
-        Assertions.assertNotNull(salvo);
-        Assertions.assertEquals(egresso.getNome(), salvo.getNome());
-        Assertions.assertEquals(egresso.getEmail(), salvo.getEmail());
-        Assertions.assertEquals(egresso.getSenha(), salvo.getSenha());
-        Assertions.assertEquals(egresso.getDescricao(), salvo.getDescricao());
-        Assertions.assertEquals(egresso.getFoto(), salvo.getFoto());
-        Assertions.assertEquals(egresso.getLinkedin(), salvo.getLinkedin());
-        Assertions.assertEquals(egresso.getInstagram(), salvo.getInstagram());
-        Assertions.assertEquals(egresso.getCurriculo(), salvo.getCurriculo());
-    }
-
 //    @Test
 //    public void deveGerarErroAoTentarSalvarEmailInvalido(){
 //        Egresso egresso = new Egresso();
@@ -124,7 +95,6 @@ public class EgressoServiceTest {
 //
 //        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.salvar(egresso), "E-mail inválido. Verifique o formato");
 //    }
-
 
     @Test
     @Transactional
@@ -171,11 +141,109 @@ public class EgressoServiceTest {
         Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.atualizar(egressoSalvo), "O e-mail já está cadastrado. Por favor, utilize um e-mail diferente");
     }
 
+//    @Test
+//    public void deveGerarErroAoTentarAtualizarEmailInvalido(){
+//        Egresso egresso = new Egresso();
+//        egresso.setNome("teste nome");
+//        egresso.setDescricao("Descricao teste");
+//        egresso.setEmail("teste");
+//
+//        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.atualizar(egresso), "E-mail inválido. Verifique o formato");
+//    }
+
+    @Test
+    @Transactional
+    public void deveGerarErroAoAtualizarUmEgressoNaoExistente() {
+        Egresso egresso = new Egresso();
+        egresso.setNome("nome teste");
+        egresso.setEmail("teste@teste.com");
+        egresso.setSenha("senha teste");
+
+        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.atualizar(egresso), "Nenhum resultado para a busca");
+    }
+
+    @Test
+    @Transactional
+    public void deveGerarErroAoBuscarPorIdVazio() {
+        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.buscarPorId(null), "ID inválido");
+    }
+
+    @Test
+    @Transactional
+    public void deveGerarErroAoBuscarPorIdInexistente() {
+        UUID idInexistente = UUID.randomUUID();
+
+        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.buscarPorId(idInexistente), "ID não encontrado");
+    }
+
+    @Test
+    @Transactional
+    public void deveGerarErroAoBuscarPorCursoInexistente() {
+        Curso curso = new Curso();
+
+        Assertions.assertThrows(BuscaVaziaRunTime.class, () -> service.buscarPorCurso(curso), "Nenhum resultado para a busca");
+    }
+
+    @Test
+    @Transactional
+    public void deveGerarErroAoBuscarPorCargoInexistente() {
+        Cargo cargo = new Cargo();
+        Assertions.assertThrows(BuscaVaziaRunTime.class, () -> service.buscarPorCargo(cargo), "Nenhum resultado para a busca");
+    }
+
+    @Test
+    @Transactional
+    public void deveGerarErroAoNaoEncontrarNenhumEgresso(){
+        Query deleteCargo = entityManager.createQuery("Delete from Cargo");
+        Query deleteDepoimento = entityManager.createQuery("Delete from Depoimento");
+        Query deleteCursoEgresso = entityManager.createQuery("Delete from CursoEgresso");
+        Query deleteEgresso = entityManager.createQuery("Delete from Egresso");
+
+        deleteCargo.executeUpdate();
+        deleteDepoimento.executeUpdate();
+        deleteCursoEgresso.executeUpdate();
+        deleteEgresso.executeUpdate();
+
+        Assertions.assertThrows(BuscaVaziaRunTime.class, () -> service.listarTodos() , "Nenhum resultado para a busca");
+    }
+
+    @Test
+    @Transactional
+    public void deveGerarErroQuandoLoginIncorreto() {
+        Assertions.assertThrows(RegraNegocioRunTime.class,
+                () -> service.efetuarLogin("loginIncorreto@example.com", "senhaIncorreta"),
+                "Erro de autenticação");
+    }
+
+    @Test
+    @Transactional
+    public void deveVerificarSalvarOEgresso() {
+        Egresso egresso = new Egresso();
+        egresso.setNome("nome teste");
+        egresso.setDescricao("Descricao teste");
+        egresso.setEmail("salvar@teste.com");
+        egresso.setSenha("senha teste");
+
+        Egresso salvo = service.salvar(egresso);
+
+        //Verificação
+        Assertions.assertNotNull(salvo);
+        Assertions.assertEquals(egresso.getNome(), salvo.getNome());
+        Assertions.assertEquals(egresso.getEmail(), salvo.getEmail());
+        Assertions.assertEquals(egresso.getSenha(), salvo.getSenha());
+        Assertions.assertEquals(egresso.getDescricao(), salvo.getDescricao());
+        Assertions.assertEquals(egresso.getFoto(), salvo.getFoto());
+        Assertions.assertEquals(egresso.getLinkedin(), salvo.getLinkedin());
+        Assertions.assertEquals(egresso.getInstagram(), salvo.getInstagram());
+        Assertions.assertEquals(egresso.getCurriculo(), salvo.getCurriculo());
+    }
+
     @Test
     @Transactional
     public void deveVerificarAtualizarOEgresso() {
 
-        Egresso egresso = repo.findById(UUID.fromString("e2ff521f-168e-4337-a9e8-2109ccee0531")).get();
+        Egresso egresso = repo.findById(UUID.fromString("e2ff521f-168e-4337-a9e8-2109ccee0531")).orElse(null);
+        assert egresso != null;
 
         egresso.setNome("nome teste atualização");
         egresso.setDescricao("Descricao teste atualização");
@@ -198,53 +266,11 @@ public class EgressoServiceTest {
 
     @Test
     @Transactional
-    public void deveGerarErroAoAtualizarUmEgressoNaoExistente() {
-        Egresso egresso = new Egresso();
-        egresso.setNome("nome teste");
-        egresso.setEmail("teste@teste.com");
-        egresso.setSenha("senha teste");
-
-        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.atualizar(egresso), "Nenhum resultado para a busca");
-    }
-
-//    @Test
-//    public void deveGerarErroAoTentarAtualizarEmailInvalido(){
-//        Egresso egresso = new Egresso();
-//        egresso.setNome("teste nome");
-//        egresso.setDescricao("Descricao teste");
-//        egresso.setEmail("teste");
-//
-//        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.atualizar(egresso), "E-mail inválido. Verifique o formato");
-//    }
-
-
-    @Test
-    @Transactional
-    public void deveGerarErroAoBuscarPorIdVazio() {
-        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.buscarPorId(null), "ID inválido");
-    }
-
-    @Test
-    @Transactional
-    public void deveGerarErroAoBuscarPorIdInexistente() {
-        UUID idInexistente = UUID.randomUUID();
-        Egresso egresso = new Egresso();
-        egresso.setId_egresso(idInexistente);
-
-        Assertions.assertThrows(RegraNegocioRunTime.class, () -> service.buscarPorId(egresso.getId_egresso()), "ID não encontrado");
-    }
-
-    @Test
-    @Transactional
     public void deveBuscarPorIdExistente() {
-        Egresso egresso = new Egresso();
-        egresso.setNome("nome teste");
-        egresso.setEmail("salvar@teste.com");
-        egresso.setSenha("senha teste");
+        Egresso egresso = repo.findById(UUID.fromString("e2ff521f-168e-4337-a9e8-2109ccee0531")).orElse(null);
+        assert egresso != null;
 
-        Egresso salvo = service.salvar(egresso);
-
-        Egresso encontrado = service.buscarPorId(salvo.getId_egresso())
+        Egresso encontrado = service.buscarPorId(egresso.getId_egresso())
                 .orElseThrow(() -> new RegraNegocioRunTime("ID não encontrado"));
 
         Assertions.assertEquals(egresso, encontrado);
@@ -252,50 +278,15 @@ public class EgressoServiceTest {
 
     @Test
     @Transactional
-    public void deveGerarErroAoBuscarPorCursoInexistente() {
-        Curso curso2 = new Curso();
-
-        Assertions.assertThrows(BuscaVaziaRunTime.class, () -> service.buscarPorCurso(curso2), "Nenhum resultado para a busca");
-    }
-
-    @Test
-    @Transactional
     public void deveBuscarPorCursoExistente() {
-        Egresso egresso = new Egresso();
-        egresso.setNome("nome teste");
-        egresso.setDescricao("Descricao teste");
-        egresso.setEmail("salvar@teste.com");
-        egresso.setSenha("senha teste");
+        Curso curso = cursoRepo.findById(UUID.fromString("0157ecab-4fb7-42b4-91ff-be4db8c759ce")).orElse(null);
+        assert curso != null;
 
-
-        Coordenador coordenador = new Coordenador();
-        coordenador.setLogin("teste");
-        coordenador.setSenha("teste");
-        coordenador.setTipo("teste");
-
-
-        Curso curso = new Curso();
-        curso.setNivel("testeNivel");
-        curso.setNome("teste");
-        curso.setCoordenador(coordenador);
-
-        CursoEgresso cursoEgresso = new CursoEgresso();
-        cursoEgresso.setEgresso(egresso);
-        cursoEgresso.setCurso(curso);
-        cursoEgresso.setAnoInicio(2024);
-
-        egresso.getCursos().add(cursoEgresso);
-
-
-        coordenadorService.salvar(coordenador);
-        cursoService.salvar(curso);
-        service.salvar(egresso);
-
-
-        List<Egresso> egressosEsperados = new ArrayList<>();
-        egressosEsperados.add(egresso);
-
-        List<Egresso> egressos = service.buscarPorCurso(cursoEgresso.getCurso());
+        List<Egresso> egressos = service.buscarPorCurso(curso);
+        List<Egresso> egressosEsperados = entityManager
+                .createQuery("SELECT e FROM Egresso e JOIN CursoEgresso ce ON e = ce.egresso WHERE ce.curso.id_curso = :id", Egresso.class)
+                .setParameter("id", UUID.fromString("0157ecab-4fb7-42b4-91ff-be4db8c759ce"))
+                .getResultList();
 
         Assertions.assertEquals(egressos, egressosEsperados);
     }
@@ -303,45 +294,17 @@ public class EgressoServiceTest {
     @Test
     @Transactional
     public void deveBuscarPorCargoExistente() {
-        Egresso egresso = new Egresso();
-        egresso.setNome("nome teste");
-        egresso.setDescricao("Descricao teste");
-        egresso.setEmail("salvar@teste.com");
-        egresso.setSenha("senha teste");
 
-        Cargo cargo = new Cargo();
-        cargo.setDescricao("teste");
-        cargo.setLocal("teste");
-        cargo.setAnoInicio(2024);
-
-        egresso.getCargos().add(cargo);
-        cargo.setEgresso(egresso);
-
-        service.salvar(egresso);
-        cargoService.salvar(cargo);
-
-        List<Egresso> egressosEsperados = new ArrayList<>();
-        egressosEsperados.add(egresso);
+        Cargo cargo = cargoRepo.findById(UUID.fromString("67c16f9b-306e-4f89-ad69-1918146f6052")).orElse(null);
+        assert cargo != null;
 
         List<Egresso> egressos = service.buscarPorCargo(cargo);
+        List<Egresso> egressosEsperados = entityManager
+                .createQuery("SELECT e FROM Egresso e JOIN Cargo c ON c.egresso = e WHERE c.id = :id", Egresso.class)
+                .setParameter("id", UUID.fromString("67c16f9b-306e-4f89-ad69-1918146f6052"))
+                .getResultList();
 
         Assertions.assertEquals(egressos, egressosEsperados);
-    }
-
-    @Test
-    @Transactional
-    public void deveGerarErroAoNaoEncontrarNenhumEgresso(){
-        Query deleteCargo = entityManager.createQuery("Delete from Cargo");
-        Query deleteDepoimento = entityManager.createQuery("Delete from Depoimento");
-        Query deleteCursoEgresso = entityManager.createQuery("Delete from CursoEgresso");
-        Query deleteEgresso = entityManager.createQuery("Delete from Egresso");
-
-        deleteCargo.executeUpdate();
-        deleteDepoimento.executeUpdate();
-        deleteCursoEgresso.executeUpdate();
-        deleteEgresso.executeUpdate();
-
-        Assertions.assertThrows(BuscaVaziaRunTime.class, () -> service.listarTodos() , "Nenhum resultado para a busca");
     }
 
     @Test
@@ -359,13 +322,8 @@ public class EgressoServiceTest {
     @Test
     @Transactional
     public void deveDeletarEgresso() {
-        Egresso egresso = new Egresso();
-        egresso.setNome("nome teste");
-        egresso.setEmail("salvar1@teste.com");
-        egresso.setSenha("senha teste");
-
-        service.salvar(egresso);
-        Assertions.assertTrue(repo.existsById(egresso.getId_egresso()));
+        Egresso egresso = repo.findById(UUID.fromString("e2ff521f-168e-4337-a9e8-2109ccee0531")).orElse(null);
+        assert egresso != null;
 
         service.deletar(egresso.getId_egresso());
         Assertions.assertFalse(repo.existsById(egresso.getId_egresso()));
@@ -374,24 +332,7 @@ public class EgressoServiceTest {
     @Test
     @Transactional
     public void deveEfetuarLogin() {
-        Egresso egresso = new Egresso();
-        egresso.setNome("nome teste");
-        egresso.setEmail("test@example.com");
-        egresso.setSenha("senha123");
-
-        service.salvar(egresso);
-
-        boolean resultado = service.efetuarLogin("test@example.com", "senha123");
-
+        boolean resultado = service.efetuarLogin("egresso1@email.com", "senha1");
         Assertions.assertTrue(resultado);
     }
-
-    @Test
-    @Transactional
-    public void deveGerarErroQuandoLoginIncorreto() {
-        Assertions.assertThrows(RegraNegocioRunTime.class,
-                () -> service.efetuarLogin("loginIncorreto@example.com", "senhaIncorreta123"),
-                "Erro de autenticação");
-    }
-
 }
