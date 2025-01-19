c417fa512c01638ac2d91e98a1cbdb08cabc662f
Breno Vidigal <96669455+FlamingDev@users.noreply.github.com>
2025-01-19 11:20:04 -0300

    Adicionado validação de senha

--- src/main/java/com/ufma/PortalEgresso/model/entity/DTOs/EgressoDTO.java
+++ src/main/java/com/ufma/PortalEgresso/model/entity/DTOs/EgressoDTO.java
@@ -1,6 +1,7 @@
 package com.ufma.PortalEgresso.model.entity.DTOs;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ public class EgressoDTO {
     private String nome;
     @Email(message = "E-mail inválido. Verifique o formato")
     private String email;
+    @Size(min = 8, message = "Senha deve ter ao menos 8 caracteres")
     private String senha;
     private String descricao;
     private String foto;
