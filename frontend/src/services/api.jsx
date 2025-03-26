26aaecee0c553b8091ea623d2e49ab846615281b
brvcelose <contatobruce90@gmail.com>
2025-03-26 07:38:30 -0300

    api url change to internal

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "portal-egresso.railway.internal", // URL do backend
+    baseURL: import.meta.env.VITE_API_URL || "https://portal-egresso.railway.internal", // URL do backend
     // http://localhost:8080
     headers: {
         'Content-Type': 'application/json',
